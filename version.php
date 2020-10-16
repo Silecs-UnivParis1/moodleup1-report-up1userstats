8ac17455730a43d75cb86d3250dfb004c0e02437
Guillaume Allegre <guillaume.allegre@silecs.info>
2020-10-16 22:03:39 +0200

    migrate custom_info_field to user_info_field

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
  *
  * @package    report
  * @subpackage up1userstats
- * @copyright  2012-2014 Silecs {@link http://www.silecs.info/societe}
+ * @copyright  2012-2020 Silecs {@link http://www.silecs.info/societe}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
